e15f0b0434f7864bcec9fb9fd015a8661e387ddb
Srihitha Kappagantula <srihithakappagantula9@gmail.com>
2026-04-16 14:45:16 +0530

    Create Pascal_triangle-1.py

---
+++ Pascal_triangle-1.py
@@ -0,0 +1,8 @@
+def generate(numRows):
+        mat=[]
+        for i in range(numRows):
+            row=[1]*(i+1)
+            for j in range(1,i):
+                    row[j]=mat[i-1][j-1]+mat[i-1][j]
+            mat.append(row)
+        return mat
